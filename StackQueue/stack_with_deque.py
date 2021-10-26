97b66459fbd56ee19301f512066330be54dfa3fd
HyeonJaeGil <now9728@kaist.ac.kr>
2021-10-27 02:25:06 +0900

    implement stack with collection::deque

--- StackQueue/stack_with_deque.py
+++ StackQueue/stack_with_deque.py
@@ -12,3 +12,43 @@ class Stack:
     def __init__(self, maxlen: int=256) -> None:
         self.capacity = maxlen
         self.__stk = deque([], maxlen)
+
+    def __len__(self)->int:
+        return len(self.__stk)
+    
+    def is_empty(self)->bool:
+        return len(self.__stk) <= 0
+    
+    def is_full(self)->bool:
+        return len(self.__stk) >= self.capacity
+    
+    def push(self, value: Any)->None:
+        self.__stk.append(value)
+    
+    def pop(self)->Any:
+        return self.__stk.pop()
+    
+    def peek(self)->None:
+        return self.__stk[-1]
+
+
+    def clear(self)->None:
+        self.__stk.clear()
+    
+    
+    def find(self, value:Any)->Any:
+        try:
+            return self.__stk.index(value)
+        except ValueError:
+            return -1 
+
+    def count(self, value:Any)->int:
+        return self.__stk.count(value)
+    
+
+    def __contains__(self, value)->bool:
+        return self.count(value)
+    
+    def dump(self)->None:
+        print(list(self.__stk))
+    
